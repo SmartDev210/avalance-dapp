f96961f52035460b4dd22a6239cb125b9b200f4c
Talent Dev <47986431+tal-athena@users.noreply.github.com>
2022-01-11 00:46:40 +0800

    remove unnecessary contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,6 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+
 const BOOToken = artifacts.require("BattleOfOlympusToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(BOOToken);
 };
